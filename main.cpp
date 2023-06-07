c028e06bb4462bf67cf5aa1daefa201ae63ad8c6
praniil <pparajuli15@gmail.com>
2023-06-07 08:33:57 +0545

    added Pong game

--- main.cpp
+++ main.cpp
@@ -2,9 +2,41 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color{ 153, 51, 255 });
+    sf::RenderWindow window(sf::VideoMode(1280, 800), "SFML works!");
+    sf::CircleShape ball(15.f);
+    ball.setFillColor(sf::Color::White);
+    ball.setPosition(640.f - 15.f, 400.f);
+
+
+    sf::RectangleShape paddleLeft(sf::Vector2f(25.f, 120.f));
+    paddleLeft.setFillColor(sf::Color::White);
+    paddleLeft.setPosition(10.f, 400.f - 60.f);
+
+    sf::RectangleShape paddleRight(sf::Vector2f(25.f, 120.f));
+    paddleRight.setFillColor(sf::Color::White);
+    paddleRight.setPosition(1270.f - 25.f, 400.f - 60.f);
+
+    sf::RectangleShape Line(sf::Vector2f(2.f, 680.f));
+    Line.setFillColor(sf::Color::White);
+    Line.setPosition(640.f - 2.f, 60.f);
+
+    //border
+    sf::RectangleShape BorderLeft(sf::Vector2f(2.f, 680.f));
+    BorderLeft.setFillColor(sf::Color::White);
+    BorderLeft.setPosition(10.f - 2.f, 60.f);
+
+    sf::RectangleShape BorderRight(sf::Vector2f(2.f, 680.f));
+    BorderRight.setFillColor(sf::Color::White);
+    BorderRight.setPosition(1280.f - 10.f - 2.f, 60.f);
+    
+    sf::RectangleShape BorderTop(sf::Vector2f(1260.f, 2.f));
+    BorderTop.setFillColor(sf::Color::White);
+    BorderTop.setPosition(10.f - 2.f, 60.f);
+
+    sf::RectangleShape BorderBottom(sf::Vector2f(1260.f, 2.f));
+    BorderBottom.setFillColor(sf::Color::White);
+    BorderBottom.setPosition(10.f - 2.f, 740.f);
+
 
     while (window.isOpen())
     {
@@ -16,7 +48,15 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(ball);
+        window.draw(paddleLeft);
+        window.draw(paddleRight);
+        window.draw(Line);
+        window.draw(BorderLeft);
+        window.draw(BorderRight);
+        window.draw(BorderTop);
+        window.draw(BorderBottom);
+
         window.display();
     }
 
